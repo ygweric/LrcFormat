53dced81d4e4a647286a15a69d3c38a30c1f7c54
电子咖啡 <ygweric@gmail.com>
2014-04-04 15:38:08 +0800

    detect CJK

--- src/com/appcpu/main/LrcFormat.java
+++ src/com/appcpu/main/LrcFormat.java
@@ -17,8 +17,8 @@ public class LrcFormat {
 		try {
 			System.out.println("lrc format --- begin");
 			String files;
-			String pathSrc="/Users/yangeric/Documents/study/subtitle/hous_of_card/";
-			String pathDir="/Users/yangeric/Documents/study/subtitle/hous_of_card_simple/";
+			String pathSrc = "/Users/yangeric/Documents/study/subtitle/hous_of_card/hous_of_card_simple/";
+			String pathDir = "/Users/yangeric/Documents/study/subtitle/hous_of_card/hous_of_card_simple2/";
 			File folderSrc = new File(pathSrc);
 			(new File(pathDir)).mkdirs();
 			File[] listOfFiles = folderSrc.listFiles();
@@ -28,28 +28,41 @@ public class LrcFormat {
 					files = listOfFiles[i].getName();
 					System.out.println(files);
 					BufferedReader br;
-					br = new BufferedReader(new FileReader(
-							pathSrc+files));
-//					br = new BufferedReader(new InputStreamReader(
-//		                      new FileInputStream(pathSrc+files), "UTF8"));
+					br = new BufferedReader(new FileReader(pathSrc + files));
+					// br = new BufferedReader(new InputStreamReader(
+					// new FileInputStream(pathSrc+files), "UTF8"));
 					BufferedWriter bw = new BufferedWriter(new FileWriter(
-							pathDir+files));
+							pathDir + files));
 					String line;
+					int j = 0;
+					String swap="";
 					while ((line = br.readLine()) != null) {
 						// process the line.
-//						System.out.println("line: " + line);
-						if (!line.contains("-->") && !isInteger(line)) {
-							bw.write(line + "\n");
+						 System.out.println(containsChinese(line)+" "+line + "\n");
+						if (!line.contains("-->") && !isInteger(line)
+								&& !line.isEmpty()) {
+							if (containsChinese(line)) {
+								swap=line;
+							} else {
+								bw.write(line + "\n");
+								bw.write(swap + "\n\n");
+							}
+							
+							
 						} else {
 							// bw.write("\n");
 						}
-
+						j++;
 					}
 					br.close();
 					bw.close();
 				}
+//				if (i>=1) {
+//					break;	
+//				}
+				
 			}
-			
+
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -65,4 +78,32 @@ public class LrcFormat {
 		// only got here if we didn't return false
 		return true;
 	}
+
+	public static boolean containsChinese(String s) {
+
+		for (int i = 0; i < s.length(); i++) {
+
+			if (isJapanese(s.charAt(i))) {
+
+				return true;
+
+			}
+
+		}
+
+		return false;
+
+	}
+
+	public static boolean isJapanese(char c) {
+
+		if (c >= '\u0100' && c <= '\uffff')
+			return true;
+
+		return false;
+
+		// simpler: return c>'\u00ff';
+
+	}
+
 }
